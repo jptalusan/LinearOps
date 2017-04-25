0a897deaf6e58f58767401c06ff0aa2335509b00
JPTalusan <jptalusan@gmail.com>
2017-04-25 23:02:27 +0800

    Starting application Added activities and layouts

---
+++ app/src/main/java/com/freelance/jptalusan/linearops/LinearEqualityActivity.java
@@ -0,0 +1,13 @@
+package com.freelance.jptalusan.linearops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LinearEqualityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_equality);
+    }
+}
--- app/src/main/java/com/freelance/jptalusan/linearops/MainActivity.java
+++ app/src/main/java/com/freelance/jptalusan/linearops/MainActivity.java
@@ -1,13 +1,33 @@
 package com.freelance.jptalusan.linearops;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.freelance.jptalusan.linearops.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.linearEquality.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, LinearEqualityActivity.class);
+                startActivity(i);
+            }
+        });
+
+        binding.linearInequality.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
